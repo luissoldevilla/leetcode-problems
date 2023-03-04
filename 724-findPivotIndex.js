cc4c5b9a54dc3002b4f15425e64447596b61d787
luissoldevilla <luis.angelsoto@hotmail.com>
2023-03-03 22:36:43 -0500

    Problem 724: Finding sum logic

--- 724-findPivotIndex.js
+++ 724-findPivotIndex.js
@@ -1,5 +1,14 @@
 function findPivotIndex(nums) {
-    console.log(nums)
+
+    let totalSum = nums.reduce((partialSum, a) => partialSum + a, 0)
+    
+    console.log(totalSum)
+    let index1 = 0
+
+    for(let i = 0; i < nums.length; i++) {
+        console.log(nums[i])
+        
+    }
 }
 
 findPivotIndex([1,7,3,6,5,6])
