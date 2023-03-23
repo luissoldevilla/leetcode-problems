da12516972542e58467a44f3f562d9f96d802fb1
luissoldevilla <luis.angelsoto@hotmail.com>
2023-03-23 16:46:16 -0400

    724 - Find Pivot Index

--- 724-findPivotIndex.js
+++ 724-findPivotIndex.js
@@ -15,11 +15,9 @@ function findPivotIndex(nums) {
     let totalSum = nums.reduce((partialSum, a) => partialSum + a, 0)
 
     console.log(totalSum)
-    let index1 = 0
 
     for(let i = 0; i < nums.length; i++) {
-        // console.log(nums[i])
-        
+        let index1 = nums[i];
     }
 }
 
