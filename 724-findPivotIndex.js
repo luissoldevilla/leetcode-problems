09e6994efd2920e215a0dc5a3b99bdc6435b20b2
luissoldevilla <luis.angelsoto@hotmail.com>
2023-04-03 07:01:49 -0400

    Java Problem Acces Code

--- 724-findPivotIndex.js
+++ 724-findPivotIndex.js
@@ -13,11 +13,12 @@
 function findPivotIndex(nums) {
 
     let totalSum = nums.reduce((partialSum, a) => partialSum + a, 0)
+    let leftIndex = 0;
 
     console.log(totalSum)
 
     for(let i = 0; i < nums.length; i++) {
-        let index1 = nums[i];
+        let index = nums[i];
         
     }
 }
