f06fdf498139754749f602dce4aebc0fe899b5fe
luissoldevilla <luis.angelsoto@hotmail.com>
2023-04-04 18:17:14 -0400

    Supper Class

---
+++ AnimalApp/src/Wolf.java
@@ -0,0 +1,10 @@
+public class Wolf extends Animal {
+    @Override
+    void makeNoise() {
+        System.out.println("Wolf: Howls");
+    }
+    @Override
+    void eat() {
+        System.out.println("Wolf: rabbits");
+    }
+}
