1764ba433487ef0ec7898735f79a222af515ef8d
luissoldevilla <luis.angelsoto@hotmail.com>
2023-04-08 11:49:27 -0400

    Acces Code Java Problem

--- accesscode.java
+++ accesscode.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class accesscode {
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.println("Enter Your name:");
+            String  firstName = input.next();
 
-        System.out.println("Enter Your name:");
-        String  firstName = input.next();
-
-        System.out.println(firstName);
+            System.out.println(firstName);
+        }
 
     }
 }
