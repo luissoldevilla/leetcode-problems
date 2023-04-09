9b1697c4ca07da5ac52dbcd79a843e242dc6d8be
luissoldevilla <luis.angelsoto@hotmail.com>
2023-04-09 00:51:40 -0400

    Java Wolf Class

--- AnimalApp/src/App.java
+++ AnimalApp/src/App.java
@@ -1,9 +1,8 @@
 
-
 public class App {
     public static void main(String[] args) {
         Wolf charlie = new Wolf();
-
+//      Functions 
         charlie.eat();
         charlie.makeNoise();
         charlie.roam();
