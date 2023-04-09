9953124830e9a815d9e33f3a7762c07b4a27a369
luissoldevilla <luis.angelsoto@hotmail.com>
2023-04-09 01:14:53 -0400

    Geek4Geeks Problem

--- find3Elements.js
+++ find3Elements.js
@@ -6,6 +6,6 @@
 // 1. Initialize the minimum value as infinity and the maximum value as -infinity
 // 2. Initialize two arrays of size n, where L[i] stores the smallest value on the left side of A[i], and R[i] stores the largest value on the right side of A[i].
 // 3. Fill the L[] array from left to right, keeping track of the minimum value.
-// Fill the R[] array from right to left, keeping track of the maximum value.
-// Traverse the array A[] from left to right and check if there exists any element A[i], such that L[i] < A[i] < R[i]. If found, return true.
-// If no such element exists, return false.
+// 4. Fill the R[] array from right to left, keeping track of the maximum value.
+// 5. Traverse the array A[] from left to right and check if there exists any element A[i], such that L[i] < A[i] < R[i]. If found, return true.
+// 6. If no such element exists, return false.
