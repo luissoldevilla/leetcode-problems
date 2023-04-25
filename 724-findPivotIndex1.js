c80d903c617f593d1e4f3c91ee525bfc13cbb793
luissoldevilla <luis.angelsoto@hotmail.com>
2023-04-25 15:11:01 -0400

    Leetcode

--- 724-findPivotIndex1.js
+++ 724-findPivotIndex1.js
@@ -5,9 +5,7 @@ function findPivotIndex(numeros) {
     console.log(totalSum)
 
     for(let i = 0; i < nums.length; i++){
-        let index = numeros[i]
-        let inde3 = numeros[i]
-
+        let index = numeros[i];
     }
 }
 findPivotIndex()
